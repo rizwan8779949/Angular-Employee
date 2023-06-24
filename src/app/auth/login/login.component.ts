6f7e249491f28087adda20929dbeacbba802fde3
Rizwan Ali <rizwan8779949@gmail.com>
2023-06-24 12:42:42 +0530

    cosmetic change in login,add logout icon

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,23 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { Router } from '@angular/router';
-import { CommonService } from 'src/app/shared/Services/common/common.service';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { SnackBarService } from 'src/app/shared/Services/snackBar/snack-bar.service';
+import { Router } from "@angular/router";
+import { CommonService } from "src/app/shared/Services/common/common.service";
+import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { SnackBarService } from "src/app/shared/Services/snackBar/snack-bar.service";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'],
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["./login.component.scss"],
 })
 export class LoginComponent implements OnInit {
   constructor(
     private snackBarService: SnackBarService,
     private formBuilder: FormBuilder,
-    private router:Router
+    private router: Router
   ) {}
   form: any;
-  isSubmitted:Boolean=false
+  isSubmitted: Boolean = false;
   ngOnInit() {
     this.form = this.formBuilder.group({
       userName: ["", Validators.required],
@@ -25,16 +25,14 @@ export class LoginComponent implements OnInit {
     });
   }
   checkLoginData() {
-    this.isSubmitted=true
+    this.isSubmitted = true;
     if (this.form.valid) {
       this.submitLoginData();
     }
   }
   submitLoginData() {
-    
-    localStorage.setItem("loginData",JSON.stringify(this.form.value))
-    this.snackBarService.success('You have Logged in Successfullly');
-this.router.navigateByUrl("home/dashboard")
-    this.form.reset();
+    localStorage.setItem("loginData", JSON.stringify(this.form.value));
+    this.snackBarService.success("You have Logged in Successfullly");
+    this.router.navigateByUrl("home/dashboard");
   }
 }
